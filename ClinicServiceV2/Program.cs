e4b6e1f63060290b9c7afdf14bcec68121363f5e
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2023-01-24 13:39:49 +0300

    add dbContext

--- ClinicServiceV2/Program.cs
+++ ClinicServiceV2/Program.cs
@@ -1,3 +1,6 @@
+using ClinicService.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ClinicServiceV2
 {
     public class Program
@@ -5,6 +8,14 @@ namespace ClinicServiceV2
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddGrpc();
+
+            builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
+            {
+                options.UseMySql(builder.Configuration["Settings:DatabaseOptions:ConnectionString"], new MySqlServerVersion(new Version(8, 0)));
+            });
+
 
             // Add services to the container.
             builder.Services.AddAuthorization();
---
+++ ClinicServiceV2/Services/ClinicService.cs
@@ -0,0 +1,78 @@
+﻿using ClinicService.Data;
+using ClinicServiceNamespace;
+using Grpc.Core;
+using static ClinicServiceNamespace.ClinicService;
+
+namespace ClinicServiceV2.Services
+{
+    public class ClinicService:ClinicServiceBase
+    {
+        private readonly ClinicServiceDbContext _context;
+        public ClinicService(ClinicServiceDbContext context)
+        {
+            _context = context;
+        }
+        public override Task<CreateClientResponse> CreateClient(CreateClientRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var client = new Client
+                {
+                    Document = request.Document,
+                    Surname = request.Surname,
+                    Firstname = request.Firstname,
+                    Patronymic = request.Patronymic
+                };
+                _context.Clients.Add(client);
+                _context.SaveChanges();
+
+                var response = new CreateClientResponse()
+                {
+                    ClientId = client.Id,
+                    ErrCode = 0,
+                    ErrMessage = "",
+                };
+                return Task.FromResult(response);
+            }
+            catch (Exception e)
+            {
+                var response = new CreateClientResponse()
+                {
+                    ErrCode = 1001,
+                    ErrMessage = "Internal server error 1001\n" + e.Message,
+                };
+                return Task.FromResult(response);
+            }
+        }
+        public override Task<GetAllClientsResponse> GetAllClients(GetAllClientsRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var clients = _context.Clients.Select(client => new ClientResponse()
+                {
+                    ClientId = client.Id,
+                    Document = client.Document,
+                    Firstname = client.Firstname,
+                    Surname = client.Surname,
+                    Patronymic = client.Patronymic
+                }).ToList();
+                var response = new GetAllClientsResponse();
+                response.Clients.AddRange(clients);
+                response.ErrCode = 0;
+                response.ErrMessage = "";
+                return Task.FromResult(response);
+
+            }
+            catch (Exception e)
+            {
+                var response = new GetAllClientsResponse()
+                {
+                    ErrCode = 1002,
+                    ErrMessage = "Internal server error 1002\n" + e.Message,
+                };
+                return Task.FromResult(response);
+            }
+
+        }
+    }
+}
